286b5cef653c53ab548e32c57fac08d8c941bd70
Ayşegül Aydoğan <aysegul.99@windowslive.com>
2022-12-10 23:43:58 +0300

    GET request to Harry Potter API/

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 import Header from './Components/UI/Header'
 
 
 const App = () => {
-  const [count, setCount] = useState(0)
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://legacy--api.herokuapp.com/api/v1/characters`)
+      console.log(result.data)
+      setItems(result.data)
+      setIsLoading(false)
+    }
+    fetchItems()
+  }, [])
   return (
     <div className="Container">
       <Header />
     </div>
   )
 }
+
+
 
 export default App
